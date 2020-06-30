aa6466691091c8d61f0c487ba2aacd5db31880f3
smolovk <smolov326@gmail.com>
2020-06-30 22:06:28 +0300

    variables validating

--- compiler/index.js
+++ compiler/index.js
@@ -12,7 +12,20 @@ fs.readFile('../test/test.sl', 'utf-8', function (error, content) {
         if (currentString.startsWith("var")) {
             var varName = currentString.replace(/var /gm, "").replace(/=.+/gs, '').trim();
             var varValue = currentString.replace(/var /gm, "").replace(varName, '').replace(/.+=/gs, '').trim()
-            console.log("variable " + varName + " = " + varValue);            
+
+            if (/[^a-zA-Z0-9]/gm.test(varName)){
+                console.error("Unacceptable symbols in variable name: " + varName);
+                return;                
+            }
+            else{
+                if(/[0-9]/gm.test(varName[0])){
+                    console.error("First character of variable name must be non-numeric: " + varName);
+                    return;                    
+                } else {
+                    console.log(varName + " = " + varValue);
+                    
+                }
+            }
         }
     }
 
--- test/test.sl
+++ test/test.sl
@@ -1,5 +1,5 @@
 var cwgvgv = "rregrg"
 var i = 10
-var 53254 = "rggetgtg"
+var к53254 = "rggetgtg"
 var smolovk = "Kirill"
 var druzhe = "Yarikk"
